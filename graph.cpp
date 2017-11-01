a27d35a0e89d31b24551d018450a25619246025c
Kirich <ivadik2323@gmail.com>
2017-11-01 18:36:01 +1000

    Add Dijkstra

--- graph.cpp
+++ graph.cpp
@@ -2,13 +2,41 @@
 
 Graph::Graph(std::string filename) {
     std::ifstream in(filename);
-    int count = 1653;
+    count = 1653;
     int index, vertex;
     double weight;
     for (int i = 0; i < count; i++) {
         in >> index >> vertex >> weight;
+        --index;
+        --vertex;
         AddEdge(index, vertex, weight);
         AddEdge(vertex, index, weight);
+    }
+}
+
+void Graph::Dijkstra(int v) {
+    std::vector<double> dist(edges.size(), inf);
+    std::vector<int> visited(edges.size(), 0);
+    std::vector<int> path(edges.size(), -1);
+    dist[v] = 0;
+    std::priority_queue<std::pair<double, int>, std::vector<std::pair<double, int>>, Compare> q;
+    visited[v] = 1;
+    q.push(std::make_pair(dist[v], v));
+
+    while (!q.empty()) {
+        std::pair<double, int> from = q.top(); q.pop();
+        for (auto edge : edges[from.second]) {
+            int to = edge.first;
+            if (!visited[to]) {
+                double tmp = from.first + edge.second;
+                if (dist[to] > tmp) {
+                    dist[to] = tmp;
+                    visited[to] = 1;
+                    path[to] = from.second;
+                    q.push(std::make_pair(dist[to], to));
+                }
+            }
+        }
     }
 }
 
@@ -19,7 +47,7 @@ void Graph::AddEdge(int index, int vertex, double weight) {
 void Graph::Print() {
     for (auto i : edges) {
         for (auto j : i.second) {
-            std::cout << j.first << " " << j.second << std::endl;
+            std::cout << i.first << " " << j.first << " " << j.second << std::endl;
         }
     }
 };
--- graph.h
+++ graph.h
@@ -2,6 +2,7 @@
 #include <unordered_map>
 #include <iostream>
 #include <fstream>
+#include <queue>
 
 class Graph {
 public:
@@ -9,4 +10,15 @@ public:
     void AddEdge(int index, int vertex, double weight);
     Graph(std::string filename);
     void Print();
+private:
+    static const long long inf = 2e9;
+    int count;
+    void Dijkstra(int v);
+};
+
+class Compare {
+public:
+    bool operator()(std::pair<double, int> a, std::pair<double, int> b) {
+        return a.first > b.first;
+    }
 };
