e1c60d1b907ba1cd21355156a735d0ac0c67147d
Arkadii Berezkin <arkm4n@gmail.com>
2018-01-14 16:41:34 +1000

    Improve parsedat script

--- parsedat.py
+++ parsedat.py
@@ -1,13 +1,27 @@
-fout = open('vlad-2009.dot', 'w')
-fin = open('vlad-2009.dat', 'r')
+import sys
+import os
 
-fout.write('graph vl-grand {\n')
+def parse():
+    if len(sys.argv) < 2: 
+        print("Please, specify filename")
+        return
+    filein = sys.argv[1]
+    fileout = os.path.splitext(filein)[0] + '.dot'
+    
+    fin = open(filein, 'r')
+    fout = open(fileout, 'w')
 
-for line in fin:
-    s = line.split()
-    u = int(s[0])
-    v = int(s[1])
-    w = float(s[2])
-    fout.write('\t{} -- {} [label = "{}"]\n'.format(v, u, w))
+    fout.write('graph G {\n')
 
-fout.write('}\n')
+    for line in fin:
+        s = line.split()
+        u = int(s[0])
+        v = int(s[1])
+        w = float(s[2])
+        fout.write('\t{} -- {} [label = "{}"]\n'.format(v, u, w))
+
+    fout.write('}\n')
+
+if __name__ == "__main__":
+    parse()
+
